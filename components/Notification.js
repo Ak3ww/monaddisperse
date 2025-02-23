ec02caf9e2d76374c8d00c77d1bd38dc98743ad2
ROLF <rakaharyadi12@gmail.com>
2025-02-23 20:11:51 +0700

    Delete components/Notification.js

--- components/Notification.js
+++
@@ -1,7 +0,0 @@
-// components/Notification.js
-
-import { ToastContainer } from 'react-toastify';
-
-export default function Notification() {
-  return <ToastContainer position="top-center" autoClose={5000} />;
-}
