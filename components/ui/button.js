ef3632994d861edea49829ba9f8e1e1da61dca46
ROLF <rakaharyadi12@gmail.com>
2025-02-23 14:08:40 +0700

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,6 +1,6 @@
 export function Button({ children, ...props }) {
   return (
-    <button className="px-4 py-2 bg-blue-500 text-white rounded" {...props}>
+    <button className="bg-blue-500 text-white px-4 py-2 rounded" {...props}>
       {children}
     </button>
   );
